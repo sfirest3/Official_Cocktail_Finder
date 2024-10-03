02b335a6fdf272d14e072374c051ceccf14840ef
sfirest3 <sfirest3@lion.lmu.edu>
2024-10-03 15:55:21 -0700

    Update Info.js

--- src/Info.js
+++ src/Info.js
@@ -9,11 +9,6 @@ export default function Info({ recipe }) {
 
   return (
     <div>
-      <img
-        src="https://img.freepik.com/free-vector/set-vector-illustration-stemware_1441-39.jpg?t=st=1727992392~exp=1727995992~hmac=32953ce8ef83d4f32c531b641dab475af25223267180f63806849468beeeda44&w=1380"
-        alt="Cocktail image"
-        className="cocktail-image"
-      />
       <h2>{recipe.strDrink}</h2>
       <img src={recipe.strDrinkThumb} alt={`${recipe.strDrink} cocktail`} />
       <h3>Ingredients</h3>
